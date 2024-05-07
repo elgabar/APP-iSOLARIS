29db4eea9306ef010cb9ff4019677b3abed8d13b
Gabriel Vieira Gaspar dos Santos <91651164+elgabar@users.noreply.github.com>
2024-05-07 14:17:41 -0300

    Add files via upload

---
+++ mobilereact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalculatorForm from './CalculatorForm';
+
+function App() {
+  return (
+    <div className="App">
+      <CalculatorForm />
+    </div>
+  );
+}
+
+export default App;
